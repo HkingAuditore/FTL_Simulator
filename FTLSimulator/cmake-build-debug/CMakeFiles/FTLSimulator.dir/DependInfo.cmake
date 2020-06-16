cc621673255d5c4521617bcf1c1f47c782b52f52
HkingAuditore <178854663@qq.com>
2020-06-16 11:47:44 +0800

    Init

---
+++ FTLSimulator/cmake-build-debug/CMakeFiles/FTLSimulator.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "F:/CProgram/FTLSimulator/FTL_Simulator/FTLSimulator/FTLSimulator/main.c" "F:/CProgram/FTLSimulator/FTL_Simulator/FTLSimulator/cmake-build-debug/CMakeFiles/FTLSimulator.dir/FTLSimulator/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FTLSimulator/cmake-build-debug/CMakeFiles/FTLSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FTLSimulator.dir/FTLSimulator/main.c.obj"
+  "FTLSimulator.exe"
+  "FTLSimulator.exe.manifest"
+  "FTLSimulator.pdb"
+  "libFTLSimulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FTLSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
